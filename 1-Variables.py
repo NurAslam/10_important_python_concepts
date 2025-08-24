077fa0e0a3bb5199ad8c50572fcca32699e9693d
Nur Aslam <hinuraslam@gmail.com>
2025-08-24 11:08:24 +0700

    upd: basicdatatypes

---
+++ 1-Variables.py
@@ -0,0 +1,5 @@
+name = "Aslam"
+age = 25
+
+print(name, age, sep=" - ")
+print("Hello, My Name is:", name)
---
+++ 2-BasicDataTypes.py
@@ -0,0 +1,9 @@
+number: int = 10 # int = integer
+decimal: float = 10.5 # float = decimal number
+text: str = "Hello World" # str = string
+active: bool = True # bool = boolean
+
+names: list = ["Aslam", "Aftizal", "Asep"] # mengisi daftar dan bisa menambah atau menghapus elemen
+coordinates: tuple = (10, 20) # sama seperti di atas tapi tidak bisa dihapus dan ditambah
+unique: set = {1, 2, 3} # set = himpunan yang tidak ada elemen yang sama dan tidak berurutan
+data: dict = {"name": "Aslam", "age": 25} # dict = dictionary (json)
